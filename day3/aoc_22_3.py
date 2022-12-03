68c00e23a2bd4e78ffe27584256706d87df807d4
Kevin Tyers <ktyers@gmail.com>
2022-12-03 10:24:12 -0700

    Day 3 code updates

--- day3/aoc_22_3.py
+++ day3/aoc_22_3.py
@@ -1,20 +1,17 @@
 from string import ascii_letters
 
 if __name__ == '__main__':
-    data = open('input.txt').read().splitlines()
-    values = {v:k for k, v in enumerate("!" + ascii_letters)}
-    # Part 1
-    priority = 0
-    for sack in data:
-        pack_size = int(len(sack)/2)
-        front, back = set(sack[0:pack_size]), set(sack[pack_size:])
-        priority += values[list(front.intersection(back))[0]]
-    print(f"part 1:{priority}")
-    # Part 2
-    priority = 0
-    i = 0
-    while i < len(data):
-        sacks = data[i:i+3]
-        priority += values[list((set.intersection(set(sacks[0]), set(sacks[1]), set(sacks[2]))))[0]]
+    all_sacks = open('input.txt').read().splitlines()
+    priorities = {v:k for k, v in enumerate("_" + ascii_letters)}
+    # First Part
+    p = 0
+    for sack in all_sacks:
+        p += priorities[set.intersection(set(sack[0:len(sack)//2]), set(sack[len(sack)//2:])).pop()]
+    print(f"1:{p}")
+    # Second Part
+    i, p = 0, 0
+    while i < len(all_sacks):
+        sacks = all_sacks[i:i+3]
+        p += priorities[set.intersection(set(sacks[0]), set(sacks[1]), set(sacks[2])).pop()]
         i += 3
-    print(f"part 2:{priority}")
+    print(f"2:{p}")
